4c11f2602be60c8c1accdf049ad2e74fd275e995
Rachel S. Lee <rslee0025@gmail.com>
2020-06-04 23:24:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,20 @@
 class Dog
   
-   
+  def name= (dogs_name)
+    @fido = dogs_name
+  end  
+
+  def name 
+    @fido
+  end    
   
+  def bark
+    puts "woof!"
 end  
 
+fido = Dog.new
+fido.name = "fido"
+
+puts fido.name
+
+fido.bark
